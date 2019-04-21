75c1fd2467cc75824bbe6b79bdf72f24a8a1391c
iceshore97 <mlkrasts@gmail.com>
2019-04-21 20:19:13 +0300

    Add the team class

--- nhl_api_access/teams.py
+++ nhl_api_access/teams.py
@@ -1,5 +1,29 @@
 import os, requests, json
 
+
+class Team:
+
+    CURRENT_DB_FIELDS = [
+        'CODE',
+        'CITY',
+        'NAME',
+        'MAIN_COLOR',
+        'SECONDARY_COLOR'
+    ]
+
+    def __init__(self, **data):
+        self.data = data
+        self.set_data()
+
+    def set_data(self):
+        for item in self.data.items():
+            pass # hmm, i haven't quite figured out what exactly I want to do...
+
+    # def update_team(self):
+
+
+
+
 raw_team_data = requests.get("https://statsapi.web.nhl.com/api/v1/teams")
 
 team_file_dir = os.path.join(os.path.abspath(os.path.join(os.path.curdir, os.path.pardir)),"teams.txt")
@@ -9,4 +33,3 @@ team_file = open(team_file_dir, "w")
 team_file.write(raw_team_data.text)
 team_file.close()
 print(raw_team_data.text)
-
