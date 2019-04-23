db2c37f8dbeea0cbf78259b22648321a2b84d0eb
iceshore97 <mlkrasts@gmail.com>
2019-04-23 22:59:25 +0300

    Add DB connection module, start properly defining Team class

--- nhl_api_access/teams.py
+++ nhl_api_access/teams.py
@@ -1,38 +1,52 @@
-import os, requests, json
+from nhl_api_access import history_database
+import sqlite3, os, requests, json
 
 
 class Team:
 
-    CURRENT_DB_FIELDS = [
-        'CODE',
-        'CITY',
-        'NAME',
-        'MAIN_COLOR',
-        'SECONDARY_COLOR'
-    ]
+    CURRENT_DB_FIELDS = {
+        'abbreviation': 'CODE',
+        'locationName': 'CITY',
+        'teamName': 'NAME',
+        # 'MAIN_COLOR',
+        # 'SECONDARY_COLOR'
+    }
 
     def __init__(self, **data):
-        for key, value in data:
-            print(key, value)
+        for key in data:
+            if key in self.CURRENT_DB_FIELDS.keys():
+                setattr(self, key, data[key])
 
-    def set_data(self):
-        for item in self.data.items():
-            pass # hmm, i haven't quite figured out what exactly I want to do...
+        self.insert_into_db()
+
+    def insert_into_db(self):
+        # If the thing exists, use UPDATE, otherwise - USE INSERT
+        check_select = "SELECT CODE FROM TEAMS WHERE ? = ?"
+        history_database.database_connection.execute(check_select, self.CURRENT_DB_FIELDS['abbreviation'], self.abbreviation)  # a little absurd, eh?
+        pass
 
     # def update_team(self):
 
 
+data_dict = {
+    'abbreviation': 'CHI',
+    'locationName': 'Chicago',
+    'teamName': 'Blackhawks'
+}
+
+blackhawks = Team(**data_dict)
 
 
-raw_team_data = requests.get("https://statsapi.web.nhl.com/api/v1/teams")
 
-jsoned_team_data = json.loads(raw_team_data)
-
-
-team_file_dir = os.path.join(os.path.abspath(os.path.join(os.path.curdir, os.path.pardir)),"teams.txt")
-print(team_file_dir)
-team_file = open(team_file_dir, "w")
-
-team_file.write(raw_team_data.text)
-team_file.close()
-print(raw_team_data.text)
+# raw_team_data = requests.get("https://statsapi.web.nhl.com/api/v1/teams")
+#
+# jsoned_team_data = json.loads(raw_team_data)
+#
+#
+# team_file_dir = os.path.join(os.path.abspath(os.path.join(os.path.curdir, os.path.pardir)),"teams.txt")
+# print(team_file_dir)
+# team_file = open(team_file_dir, "w")
+#
+# team_file.write(raw_team_data.text)
+# team_file.close()
+# print(raw_team_data.text)
